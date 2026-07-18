71517b0e1d90ff3b970b7ee73dac9c22bf18c7bd
TheOriginTube-art <TheOriginTube@gmail.com>
2026-07-18 15:44:42 +0000

    fix-nginx: use standalone certbot (stop nginx temporarily)

--- scripts/fix-nginx.sh
+++ scripts/fix-nginx.sh
@@ -2,4 +2,6 @@
 printf 'server{\nlisten 80;\nserver_name ticketflowru.ru;\nlocation /.well-known/acme-challenge/{root /var/www/html;}\nlocation /{proxy_pass http://localhost:8080;}\n}\n' > /etc/nginx/sites-available/dps-radar
 mkdir -p /var/www/html
 nginx -t && systemctl reload nginx
-certbot certonly --webroot -w /var/www/html -d ticketflowru.ru
+systemctl stop nginx
+certbot certonly --standalone -d ticketflowru.ru
+systemctl start nginx
