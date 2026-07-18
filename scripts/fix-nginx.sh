ad45987831e5f6e03e681a3bf581cc43bc3afabf
TheOriginTube-art <TheOriginTube@gmail.com>
2026-07-18 15:40:16 +0000

    fix-nginx: use --webroot instead of --nginx for certbot

--- scripts/fix-nginx.sh
+++ scripts/fix-nginx.sh
@@ -2,4 +2,4 @@
 printf 'server{\nlisten 80;\nserver_name ticketflowru.ru;\nlocation /.well-known/acme-challenge/{root /var/www/html;}\nlocation /{proxy_pass http://localhost:8080;}\n}\n' > /etc/nginx/sites-available/dps-radar
 mkdir -p /var/www/html
 nginx -t && systemctl reload nginx
-certbot --nginx -d ticketflowru.ru
+certbot certonly --webroot -w /var/www/html -d ticketflowru.ru
